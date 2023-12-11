94b637fec127c525c25e41ff2593218858659df4
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-11 22:25:51 +0000

    Migrate project to vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
