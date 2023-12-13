8799de3ef9a8778192cf6e4187631b4e0d1c2644
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-13 00:19:40 +0000

    Fix routes flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
 
 const AppRoutes: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/import" element={<Import />} />
-    </Routes>
-  </Router>
+  <Routes>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/import" element={<Import />} />
+  </Routes>
 );
 
 export default AppRoutes;
